68f6e3e88409af7e440aeb61493db09be5ea702a
Jake Wall <walljcg@gmail.com>
2025-02-18 16:15:55 +0300

    Update DSC_Analysis.py
    - stopped exporting the index

--- DSC Analysis/DSC_Analysis.py
+++ DSC Analysis/DSC_Analysis.py
@@ -93,11 +93,8 @@ def main():
         until=until_filter.isoformat(),
         patrol_type=er_patrol_type,
     )
-    print(patrols_df.head())
-    print(patrols_df.columns)
 
-
-    # download events linked with the patrol type
+    # Download events linked with the patrol type
     patrol_events = er_io.get_patrol_events(
         since=since_filter.isoformat(),
         until=until_filter.isoformat(), 
